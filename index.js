90cf11e0b75f3bb15b3bad6dbcf9d7e808111c22
Pavan0896 <pavankumar170896@gmail.com>
2024-06-26 16:10:01 +0530

    feat:full backend

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+app.use(cors());
 app.use(morgan(logFormat, { stream: accessLogStream }));
 app.use(middleware);
 app.use("/user", userRouter);
