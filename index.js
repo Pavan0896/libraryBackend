02a03b5dbd5ea141091a2d215a662ae3813de313
Pavan0896 <pavankumar170896@gmail.com>
2024-06-26 16:03:37 +0530

    Feat:full backend

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const userRouter = require("./routes/user.route");
 const libRouter = require("./routes/library.route");
 const cors = require("cors");
 const morgan = require("morgan");
-const { logFormat, accessLogStream, middleware } = require("./middleware/morgan.middleware");
+const {
+  logFormat,
+  accessLogStream,
+  middleware,
+} = require("./middleware/morgan.middleware");
 require("dotenv").config();
 const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(morgan(logFormat, { stream: accessLogStream }));
 app.use(middleware);
 app.use("/user", userRouter);
 app.use("/library", libRouter);
-
 
 app.get("/", (req, res) => {
   res.send("Health check is fine");
